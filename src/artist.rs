b61b769238b8ebd6f483ab956317719be5a30d45
Jeremy5909 <jeremyherczeg@gmail.com>
2025-10-22 13:23:42 -0700

    save to file + cli

---
+++ src/artist.rs
@@ -0,0 +1,29 @@
+use std::collections::VecDeque;
+
+use reqwest::blocking::Client;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Artists {
+    artists: VecDeque<Artist>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Artist {
+    id: String,
+    name: String,
+}
+
+pub fn search_artist(client: &Client, name: &str) -> Result<Artist, String> {
+    let url = format!("https://musicbrainz.org/ws/2/artist/?query=artist:{name}&fmt=json");
+    let result = client
+        .get(url)
+        .header(
+            "User-Agent",
+            "degrees-of-seperation/0.1 (jeremyherczeg@gmail.com)",
+        )
+        .send()
+        .map_err(|e| e.to_string())?;
+    let mut search: Artists = result.json().map_err(|e| e.to_string())?;
+    search.artists.pop_front().ok_or("Artist not found".into())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,64 @@
-use std::collections::VecDeque;
+use std::{
+    fs::File,
+    io::{Write, stdin, stdout},
+    iter,
+};
 
+use clap::Parser;
 use reqwest::blocking::Client;
-use serde::Deserialize;
 
-#[derive(Deserialize)]
-struct Artists {
-    artists: VecDeque<Artist>,
+use crate::artist::search_artist;
+
+mod artist;
+
+#[derive(Parser, Debug)]
+enum Args {
+    Load { artist: String },
+    List,
+    Save,
 }
 
-#[derive(Debug, Deserialize)]
-struct Artist {
-    id: String,
-    name: String,
-}
-
-fn search_artist(client: &Client, name: &str) -> Result<Artist, String> {
-    let url = format!("https://musicbrainz.org/ws/2/artist/?query=artist:{name}&fmt=json");
-    let result = client
-        .get(url)
-        .header(
-            "User-Agent",
-            "degrees-of-seperation/0.1 (jeremyherczeg@gmail.com)",
-        )
-        .send()
-        .map_err(|e| e.to_string())?;
-    let mut search: Artists = result.json().map_err(|e| e.to_string())?;
-    search.artists.pop_front().ok_or("Artist not found".into())
+fn read_command() -> Vec<String> {
+    print!("> ");
+    stdout().flush().unwrap();
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let input = input.trim();
+    shlex::split(input).unwrap()
 }
 
 fn main() {
-    let client = Client::new();
-    let artist = search_artist(&client, "playboi carti");
-    println!("{:#?}", artist);
+    let web_client = Client::new();
+
+    let mut save_file = File::options()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open("save.json")
+        .unwrap();
+
+    let mut local_artists = Vec::new();
+
+    loop {
+        let args = read_command();
+        let args: Vec<_> = args.iter().map(|s| s.as_str()).collect();
+
+        match Args::try_parse_from(iter::once(">").chain(args)) {
+            Ok(command) => match command {
+                Args::Load { artist } => {
+                    let artist = search_artist(&web_client, &artist).unwrap();
+                    println!("{artist:#?}");
+                    local_artists.push(artist);
+                }
+                Args::Save => match serde_json::to_writer(&mut save_file, &local_artists) {
+                    Ok(()) => println!("Saved "),
+                    Err(e) => println!("{}", e),
+                },
+                Args::List => {
+                    println!("{:#?}", local_artists);
+                }
+            },
+            Err(e) => eprintln!("{}", e.render()),
+        }
+    }
 }
